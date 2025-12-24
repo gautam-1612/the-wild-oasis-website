036da0abe7bcba8f93fd2a8231ed07f47e7aa70a
Gautam Bhushan <gautambhushan5@gmail.com>
2025-12-24 18:34:06 +0530

    Change button to submit type in SignInButton

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -3,7 +3,10 @@ import { signInAction } from "../_lib/actions";
 function SignInButton() {
   return (
     <form action={signInAction}>
-      <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
+      <button
+        type="submit"
+        className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium"
+      >
         <img
           src="https://authjs.dev/img/providers/google.svg"
           alt="Google logo"
